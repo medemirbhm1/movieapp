31b6d4732fd9a1ded6e5bebbbc2050c988a7af88
emir <me.bouhamar@esi-sba.dz>
2022-08-08 16:50:56 +0100

    finished details

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import axios from "./axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Nav from "./Nav";
+import "./scss/details.scss";
 
 function Details() {
   const { type, id } = useParams();
@@ -10,14 +11,74 @@ function Details() {
     axios
       .get(`/${type}/${id}?api_key=36213a2163c7dbc6ad1ceabef8e337cc`)
       .then((r) => {
-        console.log(r.data);
+        setDetails(r.data);
       });
   }, []);
-  return (
-    <div className="details">
+  return details ? (
+    <div
+      className="details"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${details.backdrop_path}")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div className="gradient"></div>
       <Nav />
+      <div className="container">
+        <div className="text">
+          <h1>{details.name || details.title}</h1>
+          <div className="genres">
+            {details.genres.map(({ id, name }) => (
+              <span className="genre" key={id}>
+                {name}
+              </span>
+            ))}
+          </div>
+          <div className="info">
+            <span className="rate">
+              {Number(details.vote_average).toFixed(1)}
+            </span>
+            <span className="lan">{details.original_language}</span>
+            <span>{details.first_air_date || details.release_date}</span>
+            {details.number_of_seasons ? (
+              <span>
+                {details.number_of_seasons}
+                {details.number_of_seasons > 1 ? " seasons" : " season"}
+              </span>
+            ) : null}
+          </div>
+          <p className="overview">{details.overview}</p>
+        </div>
+        {details.seasons ? (
+          <div className="sec">
+            <h2>Seasons : </h2>
+            <div className="row">
+              {details.seasons.map(
+                ({ id, poster_path, season_number, air_date }) => (
+                  <div
+                    key={id}
+                    className="image"
+                    style={{
+                      backgroundImage: `url("https://image.tmdb.org/t/p/original/${poster_path}")`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center center",
+                    }}
+                  >
+                    <div className="info">
+                      <h3>Season {season_number}</h3>
+                      <span className="airdate">{air_date}</span>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
-  );
+  ) : null;
 }
 
 export default Details;
